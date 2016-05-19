4838f5fdb7110050955bab5af5fc09d2f34374b1
Christian Grete <webmaster@christiangrete.com>
2016-05-19 13:00:54 +0200

    fix(npm): make “index.js” work - do not rely on grunt config for paths

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,25 +1,7 @@
 import * as _fs from 'fs';
-import * as _path from 'path';
-
-const _URL__GRUNT_CONFIG_FILE : string = './config/grunt.json';
-
-interface GruntConfig {
-  'FILE__TEMPLATE_MUSTACHE': string;
-  'PATH__DIST': string;
-}
-
-const _gruntConfig : GruntConfig = JSON.parse(
-    _fs.readFileSync(
-      _URL__GRUNT_CONFIG_FILE,
-      'utf8'
-    )
-  );
 
 export const TEMPLATE : string = _fs.readFileSync(
-    _path.join(
-      _gruntConfig.PATH__DIST,
-      _gruntConfig.FILE__TEMPLATE_MUSTACHE
-    ),
+    'dist/template.mustache',
     'utf8'
   );
 
