7a3a309f1066cf1768d2909c86915fb85cd9f4d4
Christian Grete <webmaster@christiangrete.com>
2016-05-24 14:26:08 +0200

    feat(grunt): add banner file

---
+++ config/banner.js
@@ -0,0 +1,7 @@
+/*!
+ * <%= pkg.name %>@<%= pkg.version %>
+ * <%= pkg.description %>
+ * <%= pkg.homepage %>
+ * Copyright (c) <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>
+ * Licensed under <%= pkg.license %> License
+ */
